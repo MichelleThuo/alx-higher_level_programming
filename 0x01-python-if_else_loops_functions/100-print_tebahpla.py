dd9ed6e8028d87b0c586721e69ca2fe2dd88f527
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-11-30 02:30:51 +0300

    Task 14: Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for i in range(122, 96, -1):
+    x = 32
+    if i % 2 == 0:
+        x = 0
+    print("{}".format(chr(i - x)), end="")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Securi
