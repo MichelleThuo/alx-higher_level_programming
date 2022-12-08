7323396bd329c0855928c27a326099d3657bdaff
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-12-08 03:58:24 +0300

    Task 12: Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,30 @@
+#!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    units = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40,
+             'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500,
+             'CM': 900, 'M': 1000}
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
+    n = 0
+    skip = False
+    if (roman_string is not None) and isinstance(roman_string, str):
+        str_len = len(roman_string)
+        for i in range(str_len):
+            base = 0
+            key = roman_string[i]
+            if (i + 1) < str_len:
+                key += roman_string[i+1]
+                base = units.get(key)
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            if not skip:
+                if base:
+                    n += base
+                    skip = True
+                    continue
+                else:
+                    key = roman_string[i]
+                    base = units.get(key)
+                    n += base
+            skip = False
+    return n
