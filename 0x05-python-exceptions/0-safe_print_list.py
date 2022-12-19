8668e3a7dd3bde898958fe3404d87754968e4aa0
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-12-19 20:16:27 +0300

    Task 0: Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements from list
+    """
+    number = 0
+    for n in range(0, x):
+        try:
+            print('{}'.format(my_list[n]), end='')
+            number += 1
+        except:
+            break
+    print()
+    return 
