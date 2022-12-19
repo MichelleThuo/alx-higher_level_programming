b3995768ef0ef6a20193ca44ecb5b9bfd9d8764e
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-12-20 01:44:45 +0300

    Task 8: Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-"""Executes a function safely.
+
+def safe_function(fct, *args):
+    
+    """Executes a function safely.
     Args:
         fct: The function to execute.
         args: Arguments for fct.
@@ -9,22 +12,10 @@ import sys
         If an error occurs - None.
         Otherwise - the result of the call to fct.
     """
-
-def safe_function(fct, *args):
-  try:
     
-    result = fct(*args)
-  except Exception as e:
-    
-    print("Exception:", e, file=sys.stderr)
-    result = None
-  return result
-
-def add(x, y):
-  return x + y
-
-result = safe_function(add, 1, 2)
-print(result)  # prints 3
-
-result = safe_function(add, "a", "b")
-print(result)
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
