aef20da01f3a1442da52457b325c4246a941b97e
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2023-01-10 21:25:42 +0300

    Task 7: Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""Script that adds all arguments to a Python list, and saves them to a file"""
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    from sys import argv
+    from os import path
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    filename = "add_item.json"
+    a_list = []
+
+    if path.exists(filename) and path.getsize(filename) > 0:
+        a_list = load_from_json_file(filename)
+
+    if len(argv) == 1:
+        save_to_json_file(a_list, filename)
+    else:
+        a_list = a_list + argv[1:]
+        save_to_json_file(a_list, filename)
