641058f46a4f2841aaa8b11e178730729feacc11
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2023-01-10 20:37:46 +0300

    Task 1: Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Defines a text file line-counting function."""
+"""Module ``1-write_file``"""
 
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """Writes a string to a text file
+
+    Args:
+        filename (str): The name of the file to write to
+        text (str): The text to write to the file
+
+    Return:
+        Number of characters written
+    """
+    if not filename or not text:
+        return 0
+
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
